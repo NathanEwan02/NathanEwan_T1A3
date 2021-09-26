2c2909e03787373c542c57b64a4007b749de48f8
Nathan <nathan.ewan14@gmail.com>
2021-09-27 09:49:31 +1000

    Adding character creator method

--- classes/character.rb
+++ classes/character.rb
@@ -1,3 +1,4 @@
+
 class Character
     attr_accessor :name, :race, :char_type
 
@@ -8,13 +9,54 @@ class Character
         @prompt = TTY::Prompt.new
     end
 
-    def character_setup
+    
+    def self.setup
+        prompt = TTY::Prompt.new
+        character_array = []
         divide = "="*100
+
         puts divide
         puts "Character Creator"
         puts divide
-        @prompt.ask("What is your characters name?", )
+
+        name = prompt.ask("What is your character's name?")
+        puts divide
+
+        character_array.append(name)
+
+        race = prompt.select("Select a character race.") do |options|
+            options.choice "Human"
+            options.choice "Elf"
+            options.choice "Dwarf"
+            options.choice "Ogre"
+        end
+        puts divide
+
+        if race == "Human"
+            character_array.append("Human")
+        elsif race == "Elf"
+            character_array.append("Elf")
+        elsif race == "Dwarf"
+            character_array.append("Dwarf")
+        elsif race == "Orge"
+            character_array.append("Ogre")
+        end
+
+        character_type = prompt.select("Select a character type.") do |types|
+            types.choice "Warrior"
+            types.choice "Mage"
+            types.choice "Paladin"
+        end
+        puts divide
+
+        if character_type == "Warrior"
+            character_array.append("Warrior")
+        elsif character_type == "Mage"
+            character_array.append("Mage")
+        elsif character_type == "Paladin"
+            character_array.append("Paladin")
+        end
+        
+        puts character_array
     end
-
-
 end
