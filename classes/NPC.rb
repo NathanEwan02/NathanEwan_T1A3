caca1af97c0a1dd0838ab520b6e93d43d08065e8
Nathan <nathan.ewan14@gmail.com>
2021-09-30 11:50:17 +1000

    Added two new functions to reduce code in main ruby file

--- classes/NPC.rb
+++ classes/NPC.rb
@@ -1,24 +1,43 @@
-class NPC 
-    attr_accessor :name, :introduction, :yes, :no
+require 'tty-prompt'
 
-    def initialize(name, introduction, yes, no)
+class NPC 
+    attr_accessor :name, :introduction, :response_1, :response_2
+
+    def initialize(name, introduction, response_1, response_2)
         @name = name
         @introduction = introduction
-        @yes = yes
-        @no = no
+        @response_1 = response_1
+        @response_2 = response_2
     end
 
-    def start_conversation()
-        prompt
+    @@prompt = TTY::Prompt.new
+
+    def give_choices(player_response1, player_response2)
+        self.class.select_option(@introduction, [player_response1, player_response2], 2) { |choice|
+            if choice == player_response1
+                puts @response_1
+            elsif choice == player_response2
+                puts @response_2
+            end
+        }
+    end
+
+    def self.select_option(text, options, num_inputs, &block)
+        
         num_inputs.to_i
-
-        choice = prompt.select(text) do |items|
-        items.per_page num_inputs
-        items.choices options
-    end
-
-    end
-
-
     
-end
+        choice = @@prompt.select(text) do |items|
+            items.per_page num_inputs
+            items.choices options
+        end
+    
+        block.call(choice)
+    end
+end 
+
+
+
+
+# me = NPC.new("Nathan", "what do you want", "NO", "OK")
+# me.give_choices("Yes", "No")
+
